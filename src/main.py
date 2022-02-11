da56e5d0f73dac636505f5927b96acf34cc629c3
Alexander R <alexanderrehnman2004@gmail.com>
2022-02-11 13:35:14 +0100

    added gobiln and fight function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 
-from resources import Character
+from resources import Character, Gobiln
+import random
+def fight(fighter : Character , enemies : list):
+
+    while not len(enemies) == 0:
+        fighter_target = random.choice(enemies)
+        fighter_target.take_damage(fighter.attack())
+        if fighter_target.get_health() == 0:
+            print("target has died!")
+            enemies.remove(fighter_target)
+            if len(enemies) == 0: break
+    
+    print(f"fight is over! {fighter.name} won! ")
+            
+
+
+
 
 def main():
+    enemies = []
+
     nick= Character("Nick", 15,3,1)
     emy= Character("Emy",20,6,5)
 
@@ -9,5 +27,13 @@ def main():
     print()
     print(emy)
 
+    enemies.append(Gobiln(1) )
+    print("\nGobilns")
+    print(enemies[0])
+
+    fight(emy, enemies)
+
 if __name__=="__main__":
     main()
+
+
--- src/resources.py
+++ src/resources.py
@@ -10,5 +10,40 @@ class Character:
         self.armor = armor
 
     def __str__(self):
-        return f"name: {self.name} \n Health:{self.health}\nDamage:{self.damage}\nArmor{self.armor}  "
+        return f"name: {self.name} \nHealth:{self.health}\nDamage:{self.damage}\nArmor{self.armor}  "
+        
+    def take_damage(self,dmg):
+        actual_damage = dmg - self.armor
+        if (self.health - actual_damage) < 0:
+            self.health= 0
+        else: self.health -= actual_damage
 
+    def attack(self):
+        return self.damage
+    
+    def get_health(self):
+        return self.health
+
+class Gobiln:
+
+
+    def __init__(self,id,):
+        self.id = id
+        self.health = 10
+        self.damage = 3
+        self.armor = 2
+
+    def __str__(self):
+        return f"ID: {self.id} \nHealth:{self.health}\nDamage:{self.damage}\nArmor{self.armor}  "
+
+    def take_damage(self,dmg):
+        actual_damage = dmg - self.armor
+        if (self.health - actual_damage) < 0:
+            self.health= 0
+        else: self.health -= actual_damage
+    
+    def attack(self):
+        return self.damage
+    
+    def get_health(self):
+        return self.health
