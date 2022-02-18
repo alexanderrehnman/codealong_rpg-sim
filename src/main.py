bba0ab234c6ef0a0e9f3eaca0376656f172d3f51
Alexander R <alexanderrehnman2004@gmail.com>
2022-02-18 13:12:58 +0100

    saved chars

--- src/main.py
+++ src/main.py
@@ -1,39 +1,68 @@
-
-from resources import Character, Gobiln
+from resources import Character, Goblin, save_character
 import random
-def fight(fighter : Character , enemies : list):
 
+def fight(fighter : Character, enemies : list):
+    
     while not len(enemies) == 0:
         fighter_target = random.choice(enemies)
         fighter_target.take_damage(fighter.attack())
         if fighter_target.get_health() == 0:
-            print("target has died!")
+            print("Target has died!")
             enemies.remove(fighter_target)
             if len(enemies) == 0: break
     
-    print(f"fight is over! {fighter.name} won! ")
-            
-
-
-
+    print(f"Fight is over! {fighter.name} won!")
+    
+def new_fight(players: list, enemies: list):
+    participants = players + enemies # Slå ihop alla deltagare till en lista
+    random.shuffle(participants)
+    
+    for char in participants:
+        target = ""
+        # Check if goblin or character
+        if char in players:
+            target = random.choice(enemies)
+        else:
+            target = random.choice(players)
+        
+        target.take_damage(char.attack())
+        if target.get_health() == 0:
+            print(f"{char.get_name()} has killed {target.get_name()}.")
+            if(type(target) == Goblin):
+                enemies.remove(target)
+            else:
+                players.remove(target)
+            participants.remove(target)
+        else:
+            print(f"{target.get_name()} was attacked by {char.get_name()}.")
+            print(f"{target.get_name()} has {target.get_health()} healthpoints left.")
+        if len(enemies) == 0 or len(players) == 0: break
+    
+    
 
 def main():
     enemies = []
-
-    nick= Character("Nick", 15,3,1)
-    emy= Character("Emy",20,6,5)
-
-    print(nick)
-    print()
-    print(emy)
-
-    enemies.append(Gobiln(1) )
-    print("\nGobilns")
-    print(enemies[0])
-
-    fight(emy, enemies)
-
+    players = []
+    
+    nick = Character("Nick", 15, 3, 1)
+    emy = Character("Emy", 20, 6, 5)
+    
+    players.append(nick)
+    players.append(emy)
+    
+    enemies.append(Goblin(1))
+    enemies.append(Goblin(2))
+    
+    # fight(emy, enemies)
+    
+    while len(enemies) != 0 and len(players) != 0:
+        new_fight(players, enemies)
+    if len(enemies) == 0:
+        print("The players won!")
+    elif len(players) == 0:
+        print("The Goblins won!")
+    
+    save_character(emy)
+    
 if __name__=="__main__":
     main()
-
-
--- src/resources.py
+++ src/resources.py
@@ -1,45 +1,50 @@
-#imports
-#global variables
-#classes
+# imports
+# global variables
+# classes
 class Character:
-
-    def __init__(self,name, health, damage, armor  ):
+    
+    def __init__(self, name, health, damage, armor):
         self.name = name
         self.health = health
         self.damage = damage
         self.armor = armor
-
+    
     def __str__(self):
-        return f"name: {self.name} \nHealth:{self.health}\nDamage:{self.damage}\nArmor{self.armor}  "
-        
-    def take_damage(self,dmg):
-        actual_damage = dmg - self.armor
-        if (self.health - actual_damage) < 0:
-            self.health= 0
-        else: self.health -= actual_damage
+        return f'Name: {self.name}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}'
 
+    def take_damage(self, dmg):
+        actual_damage = dmg - self.armor
+        if actual_damage < 0: actual_damage = 0
+        if (self.health - actual_damage) < 0: self.health = 0
+        else: self.health -= actual_damage
+    
     def attack(self):
         return self.damage
-    
+
     def get_health(self):
         return self.health
-
-class Gobiln:
-
-
-    def __init__(self,id,):
+    
+    def get_name(self):
+        return self.name
+    
+    def get_all_attributes(self):
+        return self.name, self.health, self.damage, self.armor
+    
+class Goblin:
+    
+    def __init__(self, id):
         self.id = id
         self.health = 10
         self.damage = 3
         self.armor = 2
-
+    
     def __str__(self):
-        return f"ID: {self.id} \nHealth:{self.health}\nDamage:{self.damage}\nArmor{self.armor}  "
+        return f'ID: {self.id}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}'
 
-    def take_damage(self,dmg):
+    def take_damage(self, dmg):
         actual_damage = dmg - self.armor
-        if (self.health - actual_damage) < 0:
-            self.health= 0
+        if actual_damage < 0: actual_damage = 0
+        if (self.health - actual_damage) < 0: self.health = 0
         else: self.health -= actual_damage
     
     def attack(self):
@@ -47,3 +52,26 @@ class Gobiln:
     
     def get_health(self):
         return self.health
+    
+    def get_name(self):
+        return f"Goblin #{self.id}"
+
+# functions
+def hello():
+    print("Hello World")
+    
+def save_character(character : Character):
+    """
+    Tar in karaktär, bryter ner dess attribut och sparar ner på fil.
+    Args:
+        character (Character): Det objekt som ska sparas ner på fil.
+    """
+    
+    name, health, damage, armor = character.get_all_attributes()
+    open("chars.txt", "x")
+    with open("chars.txt", "w", encoding="utf8") as f:
+        save_string = f"{name}/{health}/{damage}/{armor}\n"
+        f.write(save_string)
+        print(f"{name} has been successfully saved.")
+    
+# main code
